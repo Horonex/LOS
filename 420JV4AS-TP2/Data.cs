5b1671dc64398da9423b113ecb3be7acf1127d09
Adrien Jeanson <Adrilus3000@live.ca>
2018-10-21 22:18:47 -0400

    color&filemap

--- 420JV4AS-TP2/Data.cs
+++ 420JV4AS-TP2/Data.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace _420JV4AS_TP2
 {
@@ -16,7 +17,7 @@ namespace _420JV4AS_TP2
             { false, false, false, false, false, false, false, false, false, false },
             { false, false, false, false, false, false, false, false, false, false }};
 
-        static char[,] gridOutput = new char[,]{
+        public static char[,] gridOutput = new char[,]{
             {'?','?','?','?','?','?','?','?','?','?' },
             {'?','?','?','?','?','?','?','?','?','?' },
             {'?','?','?','?','?','?','?','?','?','?' },
@@ -28,29 +29,29 @@ namespace _420JV4AS_TP2
             {'?','?','?','?','?','?','?','?','?','?' },
             {'?','?','?','?','?','?','?','?','?','?' }};
 
-        static char vissible = ' ';
-        static char invissible = '.';
+        static char vissible = '-';
+        static char invissible = 'x';
         static char observer = 'o';
         static char obstacle = 'H';
         static char defaultC = '?';
 
 
-        static public void Init(int x,int y)
+        static public void Init(int x, int y)
         {
             gridOcuppied = new bool[x, y];
             gridOutput = new char[x, y];
-            for(int i=0;i<x;i++)
+            for (int i = 0; i < x; i++)
             {
                 for (int j = 0; j < y; j++)
                 {
-                    gridOcuppied[i,j] = false;
+                    gridOcuppied[i, j] = false;
                     gridOutput[i, j] = defaultC;
                 }
             }
 
         }
 
-        static public void SwitchOcupation(int x,int y)
+        static public void SwitchOcupation(int x, int y)
         {
             gridOcuppied[x, y] = !gridOcuppied[x, y];
             gridOutput[x, y] = obstacle;
@@ -60,26 +61,26 @@ namespace _420JV4AS_TP2
         {
             int lenght = gridOutput.GetLength(0);
             int hight = gridOutput.GetLength(1);
-            for(int i=0;i<lenght;i++)
+            for (int i = 0; i < lenght; i++)
             {
                 BressenhamOnce(ox, oy, i, 0);
-                BressenhamOnce(ox, oy, i, lenght-1);
+                BressenhamOnce(ox, oy, i, lenght - 1);
             }
-            for(int j =1;j<hight-1;j++)
+            for (int j = 1; j < hight - 1; j++)
             {
                 BressenhamOnce(ox, oy, 0, j);
-                BressenhamOnce(ox, oy, hight-1, j);
+                BressenhamOnce(ox, oy, hight - 1, j);
 
             }
             gridOutput[ox, oy] = observer;
         }
 
-        static public void BressenhamOnce(int x1, int y1, int  x2, int y2)
+        static public void BressenhamOnce(int x1, int y1, int x2, int y2)
         {
             PlotLine(x1, y1, x2, y2);
         }
 
-        static public void PlotLineLow(int x0, int y0, int x1, int y1)
+        static void PlotLineLow(int x0, int y0, int x1, int y1)
         {
             char c = vissible;
             int dx = x1 - x0;
@@ -110,10 +111,10 @@ namespace _420JV4AS_TP2
             }
 
         }
-        static public void PlotLineLow(int x1, int y1, int x0, int y0,bool rev)
-        {           
+        static void PlotLineLow(int x1, int y1, int x0, int y0, bool rev)
+        {
             char c = vissible;
-           
+
             int dx = x1 - x0;
             int dy = y1 - y0;
             int yi = 1;
@@ -133,7 +134,7 @@ namespace _420JV4AS_TP2
                 if (gridOcuppied[ax - i, ay - y])
                 {
                     c = invissible;
-                    gridOutput[ax-i, ay-y] = obstacle;
+                    gridOutput[ax - i, ay - y] = obstacle;
                 }
                 if (D > 0)
                 {
@@ -143,7 +144,7 @@ namespace _420JV4AS_TP2
                 D = D + 2 * dy;
             }
         }
-        static public void PlotLineHigh(int x0, int y0, int x1, int y1)
+        static void PlotLineHigh(int x0, int y0, int x1, int y1)
         {
             char c = vissible;
             int dx = x1 - x0;
@@ -173,7 +174,7 @@ namespace _420JV4AS_TP2
                 D = D + 2 * dx;
             }
         }
-        static public void PlotLineHigh(int x1, int y1, int x0, int y0,bool rev)
+        static void PlotLineHigh(int x1, int y1, int x0, int y0, bool rev)
         {
             char c = vissible;
             int dx = x1 - x0;
@@ -190,12 +191,12 @@ namespace _420JV4AS_TP2
             int x = x0;
             for (int i = y0; i <= y1; i++)
             {
-                if(gridOutput[ax - x, ay - i]== defaultC || gridOutput[ax - x, ay - i] == invissible)
-                gridOutput[ax-x, ay-i] = c;
-                if (gridOcuppied[ax-x,ay- i])
+                if (gridOutput[ax - x, ay - i] == defaultC || gridOutput[ax - x, ay - i] == invissible)
+                    gridOutput[ax - x, ay - i] = c;
+                if (gridOcuppied[ax - x, ay - i])
                 {
                     c = invissible;
-                    gridOutput[ax-x, ay-i] = obstacle;
+                    gridOutput[ax - x, ay - i] = obstacle;
                 }
                 if (D > 0)
                 {
@@ -206,27 +207,27 @@ namespace _420JV4AS_TP2
             }
         }
 
-        static public void PlotLine(int x0, int y0, int x1, int y1)
+        static void PlotLine(int x0, int y0, int x1, int y1)
         {
-            if( Math.Abs(y1-y0)< Math.Abs(x1 - x0))
+            if (Math.Abs(y1 - y0) < Math.Abs(x1 - x0))
             {
                 if (x0 > x1)
                 {
                     //rev
-                    PlotLineLow(x0, y0, x1, y1,true);
+                    PlotLineLow(x0, y0, x1, y1, true);
                 }
                 else
                 {
-                    
-                    PlotLineLow( x0, y0,x1, y1);
-                }   
+
+                    PlotLineLow(x0, y0, x1, y1);
+                }
             }
             else
             {
-                if(y0>y1)
+                if (y0 > y1)
                 {
                     //rev
-                    PlotLineHigh(x0, y0, x1, y1,true);
+                    PlotLineHigh(x0, y0, x1, y1, true);
                 }
                 else
                 {
@@ -238,14 +239,166 @@ namespace _420JV4AS_TP2
 
         static public void Draw()
         {
-            for(int i=0;i< gridOutput.GetLength(0);i++)
+            for (int i = 0; i < gridOutput.GetLength(0); i++)
             {
-                for(int j=0;j< gridOutput.GetLength(1);j++)
+                for (int j = 0; j < gridOutput.GetLength(1); j++)
                 {
+                    SwitchColor(j, i);
                     Console.Write(gridOutput[j, i] + " ");
+                    Console.ResetColor();
+
                 }
                 Console.WriteLine();
             }
+        }
+
+        static public void Draw(int rot)
+        {
+            int ang = rot % 4;
+
+            switch (ang)
+            {
+                case 0:
+                    {
+                        for (int i = 0; i < gridOutput.GetLength(0); i++)
+                        {
+                            for (int j = 0; j < gridOutput.GetLength(1); j++)
+                            {
+                                SwitchColor(j, i);
+                                Console.Write(gridOutput[j, i] + " ");
+                                Console.ResetColor();
+
+                            }
+                            Console.WriteLine();
+
+                        }
+                        return;
+                    }
+                case 1:
+                    {
+                        for (int i = 0; i < gridOutput.GetLength(0); i++)
+                        {
+                            for (int j = gridOutput.GetLength(1) - 1; j > 0; j--)
+                            {
+                                SwitchColor(i, j);
+                                Console.Write(gridOutput[i, j] + " ");
+                                Console.ResetColor();
+                            }
+                            Console.WriteLine();
+                        }
+                        return;
+                    }
+                case 2:
+                    {
+                        for (int i = gridOutput.GetLength(0) - 1; i > 0; i--)
+                        {
+                            for (int j = gridOutput.GetLength(1) - 1; j > 0; j--)
+                            {
+                                SwitchColor(i, j);
+                                Console.Write(gridOutput[i, j] + " ");
+                                Console.ResetColor();
+                            }
+                            Console.WriteLine();
+                        }
+                        return;
+                    }
+                case 3:
+                    {
+                        for (int i = gridOutput.GetLength(0) - 1; i > 0; i--)
+                        {
+                            for (int j = 0; j < gridOutput.GetLength(1); j++)
+                            {
+                                SwitchColor(j, i);
+                                Console.Write(gridOutput[j, i] + " ");
+                                Console.ResetColor();
+                            }
+                            Console.WriteLine();
+                        }
+                        return;
+                    }
+            }
+        }
+
+        static void SwitchColor(int x,int y)
+        {
+            if (gridOutput[x, y] == obstacle)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.BackgroundColor = ConsoleColor.DarkGray;
+            }
+            else if (gridOutput[x, y] == invissible)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.BackgroundColor = ConsoleColor.DarkYellow;
+            }
+            else if (gridOutput[x, y] == vissible)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.BackgroundColor = ConsoleColor.Yellow;
+            }
+            else if (gridOutput[x, y] == observer)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.BackgroundColor = ConsoleColor.Green;
+            }
+        }
+
+        static public void ReadObstacleMap()
+        {
+            String line;
+            int sizex;
+            int sizey;
+
+
+            //Pass the file path and file name to the StreamReader constructor
+            StreamReader sr = new StreamReader("R:\\Test.txt");
+
+            //Read the first line of text
+            sizex = int.Parse(sr.ReadLine());
+            sizey = int.Parse(sr.ReadLine());
+            Init(sizex, sizey);
+
+            for (int i = 0; i < sizex; i++)
+            {
+                line = sr.ReadLine();
+                for (int j = 0; j < sizey; j++)
+                {
+                    if (line[j] == 'x')
+                    {
+                        SwitchOcupation(j, i);
+                    }
+                }
+            }
+        }
+
+        static public void ResetObstacleMap()
+        {
+            StreamWriter sw = new StreamWriter("R:\\Test.txt");
+
+            sw.WriteLine("20");
+            sw.WriteLine("20");
+            sw.WriteLine("oooooooooooooooooooo");
+            sw.WriteLine("oooooooooooooooooooo");
+            sw.WriteLine("oooooooooooooooooooo");
+            sw.WriteLine("oooooooooooooooooooo");
+            sw.WriteLine("oooooooooooooooooooo");
+            sw.WriteLine("oooooooooooooooooooo");
+            sw.WriteLine("oooooooooooooooooooo");
+            sw.WriteLine("oooooooooooooooooooo");
+            sw.WriteLine("oooooooooooooooooooo");
+            sw.WriteLine("oooooooooooooooooooo");
+            sw.WriteLine("oooooooooooooooooooo");
+            sw.WriteLine("oooooooooooooooooooo");
+            sw.WriteLine("oooooooooooooooooooo");
+            sw.WriteLine("oooooooooooooooooooo");
+            sw.WriteLine("oooooooooooooooooooo");
+            sw.WriteLine("oooooooooooooooooooo");
+            sw.WriteLine("oooooooooooooooooooo");
+            sw.WriteLine("oooooooooooooooooooo");
+            sw.WriteLine("oooooooooooooooooooo");
+            sw.WriteLine("oooooooooooooooooooo");
+
+            sw.Close();
         }
 
     }
--- 420JV4AS-TP2/Program.cs
+++ 420JV4AS-TP2/Program.cs
@@ -1,77 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+
 
 namespace _420JV4AS_TP2
 {
 
     class Program
     {
+
         static void Main(string[] args)
         {
             var observers = new List<Pos>()
             {
                 new Pos(2,4),
-                new Pos(8,13),
-                new Pos (13,13)
+                //new Pos(8,13),
+                new Pos (11,9)
             };
-            var obstacles = new List<Pos>()
+            Data.ReadObstacleMap();
+            foreach (var p in observers)
             {
-                new Pos(1,1),
-                new Pos(1, 2),
-                new Pos(2, 1),
-                new Pos(8, 8),
-                new Pos(8, 7),
-                new Pos(7, 8),
-                new Pos(1, 8),
-                new Pos(2, 8),
-                new Pos(1, 7),
-                new Pos(8, 1),
-                new Pos(8, 2),
-                new Pos(7, 1),
-                new Pos(14, 1),
-                new Pos(14, 2),
-                new Pos(14, 3),
-                new Pos(14, 4),
-                new Pos(11,11),
-                new Pos(11,12),
-                new Pos(11,13),
-                new Pos(11,14),
-                new Pos(11,15),
-                new Pos(11,16),
-                new Pos(9,15),
-                new Pos(8,15),
-                new Pos(7,15),
-                new Pos(6,15),
-                new Pos(5,15),
-            };
-
-
-            char exit = 'a';
-            //while(exit!='e')
-            {
-
-              
-
-                //x=Console.Read();
-                //y=Console.Read();
-
-                Data.Init(20, 20);
-               
-                foreach(var p in obstacles)
-                {
-                    Data.SwitchOcupation(p.x, p.y);
-                }
-
-                foreach(var p in observers)
-                {
-                    Data.BressenhamAll(p.x,p.y);
-                }
-                Data.Draw();
-
-                int s = Console.Read();
-                exit = (char)s;
-                
+                Data.BressenhamAll(p.x, p.y);
             }
+            Data.Draw(0);
+            Console.ReadKey();
+
+
         }
     }
 }
